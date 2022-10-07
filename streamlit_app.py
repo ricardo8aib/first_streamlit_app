9d0b0267a39c266786f083ed8be2fa602441520f
Ricardo Ibarra <85208152+ricardo8aib@users.noreply.github.com>
2022-10-07 09:37:18 -0500

    Add breakfast menu header and 4 lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
-streamlit.title("My parents new healthy diner")
+streamlit.title("My Mom's New Healthy Diner")
 
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑 Avocado Toast")
