e39746e02ba3e94e92d4987a7e7d07675874eb2a
Ricardo Ibarra <85208152+ricardo8aib@users.noreply.github.com>
2022-10-13 20:57:11 -0500

    Convert API results into a pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Convert API call results into a Pandas DataFrame
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
